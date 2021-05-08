dea4e28cfd51b867b865ece9d29cf78ca66c7335
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-05-08 20:45:37 +0900

    Initial commit from Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
